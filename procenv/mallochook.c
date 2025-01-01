6073f91856fc4d60b7eefbbafebea71cb3c6ca09
Chun-Ying Huang <huangant@gmail.com>
2025-01-01 16:24:43 +0800

    fix errors due to mismatched glibc version

--- procenv/mallochook.c
+++ procenv/mallochook.c
@@ -1,16 +1,21 @@
 #include <malloc.h>
 #include <stdio.h>
 
-void my_hook(size_t size, const void *caller) {
+void* my_hook(size_t size, const void *caller) {
 	printf("hook: size %zu caller %p\n", size, caller);
+	return NULL;
 }
 
-//extern void *(*__malloc_hook)(size_t size, const void *caller);
+extern void * (*__malloc_hook)(size_t size, const void *caller);
 
 int main() {
 	char *buf = NULL;
 	
+#if defined(__GLIBC__) && !defined(__UCLIBC__) && __GLIBC__ == 2 && __GLIBC_MINOR__ < 34
 	__malloc_hook = my_hook;
+#else
+	fprintf(stderr, "__malloc_hook is only available before glibc 2.34 (exclusive)\n");
+#endif
 	buf = malloc(64);
 	printf("hello %p\n", buf);
 
